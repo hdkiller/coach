ca07eb9a0a884eb168114f3c6a2af47369e959ee
Laszlo Racz <lracz@newpush.com>
2025-12-02 10:59:03 -0500

    feat(prisma): add ai analysis fields and migrations to workout model

---
+++ prisma/migrations/20251202145605_add_workout_ai_analysis/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Workout" ADD COLUMN     "aiAnalysis" TEXT,
+ADD COLUMN     "aiAnalyzedAt" TIMESTAMP(3);
---
+++ prisma/migrations/20251202154918_add_workout_analysis_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Workout" ADD COLUMN     "aiAnalysisStatus" TEXT DEFAULT 'NOT_STARTED';
